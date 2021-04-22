bf1edc649da428d10938dc4f7e95d713c865deb3
ptraversi <ptraversi@gmail.com>
2021-04-22 15:11:37 -0700

    feat: linear search

---
+++ test/search/linear.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+
+#include "../../src/search/linear.h"
+
+void arrcpy(int to[], int from[], int n) {
+  for (int i = 0; i < n; i++) {
+    to[i] = from[i];
+  }
+}
+
+void test_linear_search(void) {
+  // Should work as expected on int[]
+  int arr[] = {1, 2, 3, 4, 5};
+  assert(linear_search(arr, 3, 5) == 2);
+
+  assert(linear_search(arr, 6, 5) == -1);
+
+  // Should work as expected on char[]
+  char list[] = "hello";
+  assert(linear_search(list, 'l', 5) == 2);
+
+  assert(linear_search(list, 'a', 5) == -1);
+}
