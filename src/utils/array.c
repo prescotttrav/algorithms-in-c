04d6a1fd73b2c830e48fe46ad79aaab4aa5a019c
Prescott J. Traversi <46552614+prescotttrav@users.noreply.github.com>
2021-04-26 17:39:17 -0500

    fix: queue implementation (#12)
    * feat: iterative quick sort implementation
    
    * fix: update code for clarity & share some functionality
    
    * fix: include static declaration to functions
    
    * fix: update queue to data structure
    
    * fix: rename file structure
    
    * fix: minor code updates
    
    * fix: memory issues

--- src/utils/array.c
+++ src/utils/array.c
@@ -1,6 +1,6 @@
 #include "array.h"
 
-/* swap_int switches two int array items in place */
+/* swap_int switches two int array items */
 void swap_int(int *a, int i, int j) {
   int temp = *(a + i);
   *(a + i) = *(a + j);
